d93677aa3c59986cb04d91af82718a41921bd4c9
Lucat22 <Lucalvins1@gmail.com>
2024-04-08 21:24:35 -0300

    Exercitando os slides

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+var num = 1;
+num = 3;
+var price = 1.5
+var myName = 'Lucas Alves';
+var trueValue = true;
+var nullVar = null;
+var und;
+
+
+
+
+
+
+console.log('num: ' + num);
+console.log('Meu Nome: ' + myName);
+console.log('trueValue: ' + trueValue);
+console.log('price: ' + price);
+console.log('nullVar: ' + nullVar);
+console.log('und: ' + und);
---
+++ js/tempCodeRunnerFile/tempCodeRunnerFile.js
@@ -0,0 +1,88 @@
+// var num = 1;
+// if (num === 1) {
+//     console.log('num is equal to 1');
+// }
+
+// var num = 0;
+// if (num === 1) {
+//     console.log('num is egual to 1');
+// } else {
+//     console.log('num is not equal to 1, the value of num is: ' + num);
+// }
+
+// var month = 5;
+// if (month === 1) {
+//     console.log('January');
+// } else if (month === 2) {
+//     console.log('February');
+// } else if (month === 3) {
+//     console.log('March');
+// } else {
+//     console.log('Month is not January, February or March');
+// }
+
+// let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// for (var i=0; i < numbers.length; i++) {
+//     numbers[i] = numbers[i+1];
+// }
+
+// console.log(numbers);
+
+// const zero = 0;
+// const positiveNumbers = [1, 2, 3];
+// const negativeNumbers = [-3, -2, -1];
+// let numbers = negativeNumbers.concat(zero, positiveNumbers);
+
+// console.log(numbers.join(', '));
+
+// function isEven(x){
+//     console.log(x);
+//     return (x % 2 === 0) ? true : false;
+// }
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+
+// numbers.some(isEven);
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+// numbers.forEach( x => console.log( x % 2 === 0));
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+// function isEven(x){
+//     return (x % 2 === 0) ? true : false;
+// }
+
+
+// const evenNumbers = numbers.filter(isEven);
+
+// console.log(evenNumbers);
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+// numbers.reverse();
+// console.log('Array numbers com reverse: ' + numbers.join(', '));
+
+// numbers.sort();
+// console.log('Array numbers com sort: ' + numbers.join(', '));
+
+// function compare(a, b) {
+//     if (a < b) {
+//         return -1;
+//     }
+//     if (a > b) {
+//         return 1;
+//     }
+
+//     return 0;
+// }
+// numbers.sort(compare);
+// console.log('Array numbers executando a função compare: ' + numbers.join(', '));
+
+// let names = ['Maria', 'Angelica', 'Luiza', 'Kelly', 'Rafaela', 'Marcela', 'Beatriz']
+
+// console.log(names.sort().join(', '));
+
